8303c0184a3a93b7abb24e970a89c39627b67fdf
Carlos Henrique <chsjr1996@gmail.com>
2020-05-08 22:56:09 -0300

    Add CRUD operations for reviews module

---
+++ src/controllers/ReviewController.ts
@@ -0,0 +1,68 @@
+import { Response } from 'express';
+import {
+  JsonController,
+  UseBefore,
+  Res,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Body,
+} from 'routing-controllers';
+import AuthMiddleware from '../middlewares/AuthMiddleware';
+import ReviewModel from '../models/ReviewModel';
+import Responses from '../utils/builders/Responses';
+import AppError from '../utils/helpers/AppError';
+
+@JsonController('/reviews')
+@UseBefore(AuthMiddleware)
+export default class ReviewController {
+  /**
+   * CRUD operations
+   */
+
+  // TODO: Type req body
+  @Post()
+  public async create(@Body() req: any, @Res() res: Response) {
+    const review = await ReviewModel.create(req);
+    return Responses.Success(res, { review });
+  }
+
+  @Get()
+  public async getAll(@Res() res: Response) {
+    const reviews = await ReviewModel.find();
+    return Responses.Success(res, { reviews });
+  }
+
+  @Get('/:id')
+  public async getOne(@Param('id') id: string, @Res() res: Response) {
+    const review = await ReviewModel.findById(id);
+    if (!review) throw new AppError('Review not found!', 404);
+    return Responses.Success(res, { review });
+  }
+
+  @Patch('/:id')
+  public async update(
+    @Param('id') id: string,
+    @Body() body: any,
+    @Res() res: Response
+  ) {
+    const review = await ReviewModel.findByIdAndUpdate(id, body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!review) throw new AppError('Review not found!', 404);
+    return Responses.Success(res, { review });
+  }
+
+  @Delete('/:id')
+  public async delete(@Param('id') id: string, @Res() res: Response) {
+    const review = await ReviewModel.findOneAndUpdate(
+      { _id: id, active: { $ne: false } },
+      { active: false }
+    );
+    if (!review) throw new AppError('Review not found!', 404);
+    return Responses.Success(res, null, 'Review has been deleted!');
+  }
+}
---
+++ src/models/ReviewModel.ts
@@ -0,0 +1,86 @@
+import mongoose, { Schema, Model, Document, HookNextFunction } from 'mongoose';
+import IModel from './IModel';
+
+interface IReviewSchema extends Document {
+  review: string;
+  rating: number;
+  createdAt: string;
+  tour: string;
+  user: string;
+  active: boolean;
+}
+
+class ReviewModel implements IModel {
+  constructor() {
+    this.schema = this.initSchema();
+    this.hooks();
+    this.model = this.initModel();
+  }
+
+  public schema: Schema<IReviewSchema>;
+  public model: Model<IReviewSchema>;
+
+  public initSchema() {
+    return new mongoose.Schema(
+      {
+        review: {
+          type: String,
+          required: true,
+        },
+        rating: {
+          type: Number,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now(),
+        },
+        tour: {
+          type: mongoose.Types.ObjectId,
+          ref: 'Tour',
+          required: true,
+        },
+        user: {
+          type: mongoose.Types.ObjectId,
+          ref: 'User',
+          required: true,
+        },
+        active: {
+          type: Boolean,
+          default: true,
+          select: false,
+        },
+      },
+      {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+      }
+    );
+  }
+
+  public hooks() {
+    // Populate relationship
+    this.schema.pre<IReviewSchema>('/^find/', function (
+      next: HookNextFunction
+    ) {
+      this.populate({
+        path: 'user',
+        select: 'name photo',
+      });
+      next();
+    });
+
+    // Not show inactive
+    this.schema.pre<Model<IReviewSchema>>(/^find/, function (
+      next: HookNextFunction
+    ) {
+      this.find({ active: { $ne: false } });
+      next();
+    });
+  }
+
+  public initModel() {
+    return mongoose.model<IReviewSchema>('Review', this.schema);
+  }
+}
+
+export default new ReviewModel().model;
